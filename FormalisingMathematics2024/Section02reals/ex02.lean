fd17a7f8498ee8c42384d0ba608444d5548717b1
Archie Browne <archibald.browne21@imperial.ac.uk>
2024-01-25 22:12:44 +0000

    attempting mono_bounded_conv

--- FormalisingMathematics2024/Section02reals/ex02.lean
+++ FormalisingMathematics2024/Section02reals/ex02.lean
@@ -9,6 +9,7 @@ def sum (a : ℕ → ℝ) (n : ℕ) : ℝ := ∑ i in range n, a i
 def abs_sum (a : ℕ → ℝ) (n : ℕ) : ℝ := ∑ i in range n, |a i|
 /- a sequence of real numbers converges if it has a limit -/
 def converges (a : ℕ → ℝ) : Prop := ∃ L, TendsTo a L
+/- a sequence of real numbers is cauchy if its terms become, and remain, arbitrarily close -/
 def cauchy (a : ℕ → ℝ) : Prop := ∀ ε > 0, ∃ (N : ℕ), ∀ (n m : ℕ), (n ≥ N ∧ m ≥ N → |a n - a m| < ε)
 /- a sum converges if the sequence of partial sums converges -/
 def sum_conv (a : ℕ → ℝ) : Prop := converges (sum a)
@@ -28,6 +29,12 @@ lemma converges_def (a : ℕ → ℝ) : converges a ↔ ∃ L, TendsTo a L := by
 lemma sum_conv_def (a : ℕ → ℝ) : sum_conv a ↔ converges (sum a) := by rfl
 lemma sum_abs_conv_def (a : ℕ → ℝ) : sum_abs_conv a ↔ converges (abs_sum a) := by rfl
 lemma cauchy_def (a : ℕ → ℝ) : cauchy a ↔ ∀ ε > 0, ∃ (N : ℕ), ∀ (n m : ℕ), (n ≥ N ∧ m ≥ N → |a n - a m| < ε) := by rfl
+
+/- any term in a sequence can be represented as a telescoping sum -/
+lemma sum_range_succ_sub (a : ℕ → ℝ) (n : ℕ) : a n = sum a (n + 1) - sum a n := by
+  dsimp [sum_def]
+  rw [eq_sub_iff_add_eq]
+  exact (sum_range_succ_comm a n).symm
 
 /- if a sequence is cauchy, then it is bounded -/
 theorem cauchy_bounded (a : ℕ → ℝ) : cauchy a → Bounded a := by
@@ -60,7 +67,34 @@ theorem cauchy_bounded (a : ℕ → ℝ) : cauchy a → Bounded a := by
       gcongr
       apply hM
       exact Nat.lt.base N}
+#check @le_sSup
 
+/- a sequence converges if it is monotone and bounded -/
+lemma mono_bounded_conv (a : ℕ → ℝ) : Monotone a ∧ Bounded a → converges a := by
+  intro ⟨hM, hB⟩
+  let L := sSup {a i | i : ℕ}
+  use L
+  intro ε hε
+  have : ∃ N, a N > L - ε := by
+    · sorry
+  obtain ⟨N, hN⟩ := this
+  use N
+  intro n hn
+  rw [abs_sub_lt_iff]
+  constructor
+  · dsimp
+    rw [sub_lt_iff_lt_add]
+    calc a n ≤ sSup {x | ∃ i, a i = x} := by {
+      have : a n ∈ {x | ∃ i, a i = x} := by sorry
+      exact le_sSup this
+    }
+    _ < ε + sSup {x | ∃ i, a i = x} := by exact lt_add_of_pos_left (sSup {x | ∃ i, a i = x}) hε
+  · rw [sub_lt_comm]
+    calc L - ε < a N := by exact hN
+    _ ≤ a n := by exact hM hn
+
+
+/- for real numbers, cauchy and convergence criterion are equivalent -/
 theorem cauchy_iff_convergent (a : ℕ → ℝ) : converges a ↔ cauchy a := by
   constructor
   <;> intro h
@@ -71,43 +105,33 @@ theorem cauchy_iff_convergent (a : ℕ → ℝ) : converges a ↔ cauchy a := by
     rintro n m ⟨hn, hm⟩
     calc |a n - a m| = |(a n - L) + (L - a m)| := by ring_nf
     _ ≤ |a n - L| + |L - a m| := abs_add (a n - L) (L - a m)
-    _ < ε / 2 + ε / 2 := by {
-      gcongr
-      · exact hB n hn
-      · rw [abs_sub_comm]
-        exact hB m hm}
+    _ < ε / 2 + ε / 2 := by
+      · gcongr
+        · exact hB n hn
+        · rw [abs_sub_comm]
+          exact hB m hm
     _ = ε := by ring_nf
-  · let b (n : ℕ) := sSup {a i | i ≥ n}
+  · obtain ⟨A, hA⟩ := cauchy_bounded a h -- maybe dont need this direction, just stick with one way.
+    let b (n : ℕ) := - sSup {a i | i ≥ n}
     have bMon : Monotone b := by sorry
-    let L := sInf {b i | i : ℕ}
+    have bBounded : Bounded b := by sorry
+    obtain ⟨L, hL⟩ := mono_bounded_conv b ⟨bMon, bBounded⟩
     use L
     intro ε hε
     obtain ⟨N, hN⟩ := h (ε / 2) (by linarith)
+    use N
+    intro n hn
+
 
     sorry
 
-#check sSup
-/- a sequence converges if it is monotone and bounded -/
-lemma mono_bounded_conv (a : ℕ → ℝ) : Monotone a ∧ Bounded a → converges a := by
-  intro ⟨hM, hB⟩
-  by_contra h
-  have : ¬ Bounded a := by {
-    intro ⟨R, hR⟩
-    rw [converges_def] at h
-    push_neg at h
-    specialize h R
-
-    sorry
-  }
-  contradiction
-
+/- two sums of the same terms can be subtracted and represented as one sum -/
 lemma sum_sub_range_sub (m n : ℕ) (h : m ≤ n) (f : ℕ → ℝ) :
   ∑ x in range n, f x - ∑ x in range m, f x = ∑ x in range (n - m), f (m + x) := by
   refine tsub_eq_of_eq_add ?h
   obtain ⟨t, ht⟩ := Nat.exists_eq_add_of_le h
   rw [ht, Nat.add_sub_self_left m t, sum_range_add f m t]
   ring
-
 
 /- absolute convergence implies convergence -/
 lemma abs_conv : ∀ (a : ℕ → ℝ), sum_abs_conv a → sum_conv a := by
@@ -140,30 +164,30 @@ lemma sum_conv_zero : ∀ (a : ℕ → ℝ), sum_conv a → TendsTo a 0 := by
   obtain ⟨N, hN⟩ := hL (ε / 2) (by linarith)
   use N
   intro n hn
-  rw [sub_zero]
-  have : a n = sum a (n + 1) - sum a n := by -- TURN INTO LEMMA?
-    · dsimp [sum_def]
-      rw [eq_sub_iff_add_eq]
-      exact (sum_range_succ_comm a n).symm
-  rw [this]
+  rw [sub_zero, sum_range_succ_sub a n]
   calc |sum a (n + 1) - sum a n| = |(sum a (n + 1) - L) - (sum a n - L)| := by ring_nf
   _ ≤ |sum a (n + 1) - L| + |sum a n - L| := by exact abs_sub (sum a (n + 1) - L) (sum a n - L)
   _ < ε / 2 + ε / 2 := by
     · gcongr
-      exact hN (n + 1) (by linarith)
-      exact hN n hn
+      · exact hN (n + 1) (by linarith)
+      · exact hN n hn
   _ = ε := by linarith
 
-/- a n nonnegative iff the partial sums are monotone increasing -/
+/- a sequence is nonnegative iff the partial sums are monotone increasing -/
 lemma partial_monotone (a : ℕ → ℝ) : (∀ (n : ℕ), a n ≥ 0) ↔ Monotone (sum a) := by
   constructor
   · intro h x y hxy
     dsimp [sum_def]
-  -- need a way to split the range of summation :
-    -- have : ∀ (r s : ℕ), (r ≤ s) → ∑ i in range r, a i + ∑ i in range (r - s, s), a i = ∑ i in range s, a i := by sorry OR:
-    -- have : ∀ (r s : ℕ), (r ≤ s) → ∑ i in range r, a i + ∑ i in range (r - s), a (i + r) = ∑ i in range s, a i := by sorry
-    sorry
-  sorry
+    refine sub_nonneg.mp ?_
+    rw [sum_sub_range_sub x y hxy a]
+    refine sum_nonneg ?_
+    intro i _
+    exact h (x + i)
+  · intro h n
+    specialize h (Nat.le_add_right n 1)
+    rw [sum_range_succ_sub a n]
+    linarith
+
 /- The Comparison Test: if an ≤ bn and the sum of bn's converges, then the same is true for an -/
 theorem comparison_test (a b : ℕ → ℝ) (ha : ∀ n, a n ≥ 0) (hab : ∀ n, a n ≤ b n) (hb : sum_conv b) : sum_conv a := by
   rw [sum_conv_def]
@@ -173,33 +197,31 @@ theorem comparison_test (a b : ℕ → ℝ) (ha : ∀ n, a n ≥ 0) (hab : ∀ n
   · obtain ⟨M, hM⟩ := hb
     use M
     intro n
-    have hbM : sum b n ≤ M := by {
-      -- ASK ABOUT IN CLASS
-      by_contra hc
-      obtain ⟨B, hB⟩ := hM (|(M + (sum b n)) / 2 - M|) (by (
-        refine abs_pos.mpr ?_
-        intro hf
-        linarith))
-      specialize hB (B + n) (by linarith)
-      have hbMono : Monotone (sum b) := by {
-        rw [← partial_monotone b]
-        intro m
-        exact le_trans (ha m) (hab m)}
-      have : sum b (B + n) < sum b n := by {
-        sorry}
-      unfold Monotone at hbMono
-      specialize hbMono (Nat.le_add_right n B)
-      rw [lt_iff_not_le, add_comm] at this
-      contradiction}
+    have hbM : sum b n ≤ M := by
+      · by_contra hc
+        obtain ⟨B, hB⟩ := hM (|(M + (sum b n)) / 2 - M|) (by (
+          refine abs_pos.mpr ?_
+          intro hf
+          linarith))
+        specialize hB (B + n) (by linarith)
+        have hbMono : Monotone (sum b) := by
+          · rw [← partial_monotone b]
+            intro m
+            exact le_trans (ha m) (hab m)
+        have : sum b (B + n) < sum b n := by {
+          sorry}
+        unfold Monotone at hbMono
+        specialize hbMono (Nat.le_add_right n B)
+        rw [lt_iff_not_le, add_comm] at this
+        contradiction
     rw [sum_def] at *
     calc
       |∑ i in range n, a i| = ∑ i in range n, a i := abs_sum_of_nonneg' ha
-      _ ≤ ∑ i in range n, b i := by {
-        gcongr with r hr
-        · exact hab r}
+      _ ≤ ∑ i in range n, b i := by
+        · gcongr with r hr
+          · exact hab r
       _ ≤ M := hbM
 
--- algebra of limits ??
 /- The Sandwich Test: if cn ≤ an ≤ bn and the sums of both the cn and bn converges, then the same
 is true for an  -/
 theorem sandwich_test (a b c : ℕ → ℝ) (hca : ∀ n, c n ≤ a n) (hab : ∀ n, a n ≤ b n) (hc : sum_conv c)
