56f339d987ea54029285bfe6e561ff26988790cd
Archie Browne <archibald.browne21@imperial.ac.uk>
2024-01-28 21:03:26 +0000

    adding comments

--- FormalisingMathematics2024/Section02reals/ex02.lean
+++ FormalisingMathematics2024/Section02reals/ex02.lean
@@ -25,7 +25,7 @@ def Bounded (a : ℕ → ℝ) : Prop := ∃ M, ∀ n, |a n| ≤ M
 #eval ∑ i in range 11, i -- `55`
 -- **Outcome** : this is fine (eval command is not used for this)
 
-@[simp]
+@[simp] -- tell these definitions to the simplifier
 lemma tendsTo_def {a : ℕ → ℝ} {t : ℝ} : -- **From K.Buzzard**
     TendsTo a t ↔ ∀ ε, 0 < ε → ∃ B : ℕ, ∀ n, B ≤ n → |a n - t| < ε := by rfl
 lemma sum_def (a : ℕ → ℝ) (n : ℕ) : sum a n = ∑ i in range n, a i := by rfl
@@ -37,80 +37,118 @@ lemma cauchy_def (a : ℕ → ℝ) : cauchy a ↔ ∀ ε > 0, ∃ (N : ℕ), ∀
 
 /- any term in a sequence can be represented as a telescoping sum -/
 lemma sum_range_succ_sub (a : ℕ → ℝ) (n : ℕ) : a n = sum a (n + 1) - sum a n := by
+/- this lemma is easily deducible from the library, but is use a ocuple of times,
+so it is useful to give it in a lemma -/
   dsimp [sum_def]
+/- once lean knows `sum` is just a finite `∑`, the proof is short -/
   rw [eq_sub_iff_add_eq]
   exact (sum_range_succ_comm a n).symm
 
 /- if a sequence is cauchy, then it is bounded -/
 theorem cauchy_bounded (a : ℕ → ℝ) : cauchy a → Bounded a := by
   intro h
+/- we can bound the distance between terms by 1 after a certain `N` -/
   obtain ⟨N, hN⟩ := h 1 (by norm_num)
   specialize hN N
+/- `max'` of a set in `ℝ` gives the maximum as an element of `ℝ` when provided
+with a proof that the set is non empty  -/
   let M := max' (image (fun i ↦ |a i|) (range (N + 1))) (by norm_num)
-  have hM (r : ℕ) (hr : r < N + 1) : |a r| ≤ M := by {
-    apply le_max'
-    rw [mem_image]
-    use r
-    rw [mem_range]
-    exact ⟨hr, rfl⟩}
+/- `M` is the maximum of {a₁, ⋯, aₙ}, so for any of these elements, `M` is greater -/
+  have hM (r : ℕ) (hr : r < N + 1) : |a r| ≤ M := by
+    · apply le_max'
+      rw [mem_image]
+      use r
+      rw [mem_range]
+      exact ⟨hr, rfl⟩
+/- we would now like to bound aₙ for all n. `M + 1` should work, since after `N`
+the distance between terms is less than 1, and before it, all terms are less than `M` -/
   use M + 1
   intro n
+/- `n` is either smaller than `N`, or at least `N`, we split into the two cases -/
   cases' (lt_or_ge n N) with _ _
-  · calc |a n| ≤ M := by {
-    apply hM
-    exact Nat.lt_add_right 1 (by assumption)}
+/- when `n < N`, `hM` provides a proof that `|a n| < M` -/
+  · calc |a n| ≤ M := by
+          · apply hM
+            exact Nat.lt_add_right 1 (by assumption)
     _ ≤ M + 1 := by linarith
+/- when `n ≥ N`, we use the triangle inequality with `a N` which we know is bounded by `M` -/
   · calc |a n| = |a N + (a n - a N)| := by ring_nf
     _ ≤ |a N| + |a n - a N| := by exact abs_add _ _
-    _ ≤ |a N| + 1 := by {
-      gcongr
-      specialize hN n ⟨le_refl N, by assumption⟩
-      apply le_of_lt
-      rw [abs_sub_comm]
-      exact hN}
-    _ ≤ M + 1 := by {
-      gcongr
-      apply hM
-      exact Nat.lt.base N}
+    _ ≤ |a N| + 1 := by
+/- `x ≤ y → z + x ≤ z + y` and so we can apply this fact -/
+      · apply add_le_add_left
+        specialize hN n ⟨le_refl N, by assumption⟩
+        apply le_of_lt
+        rw [abs_sub_comm]
+        exact hN
+    _ ≤ M + 1 := by
+/- similarly, `x ≤ y → x + z ≤ y + z`, so it is enough to prove that `x ≤ y` -/
+      · apply add_le_add_right
+        apply hM
+        exact Nat.lt.base N
+
 
 /- a sequence converges if it is monotone and bounded -/
 lemma mono_bounded_conv (a : ℕ → ℝ) : Monotone a ∧ Bounded a → converges a := by
   intro ⟨hM, hB⟩
+/- if a sequence is monotone and bounded, then it converges to the supremum of its terms -/
   let L := sSup {a i | i : ℕ}
   use L
   intro ε hε
+/- by definition of `L`, the supremum, if we take any number smaller than it,
+we can find an element of the set larger than this new value -/
   have : ∃ N, a N > L - ε := by
-    · have : Set.Nonempty ({x | ∃ i, a i = x}) := by
+/- to prove this, we of course need that the set is non empty, which is easily verifiable -/
+    · have : {x | ∃ i, a i = x}.Nonempty := by
         · use (a 1); use 1
-      obtain ⟨aN, ⟨⟨N, hN⟩, _⟩⟩ := @Real.add_neg_lt_sSup ({x | ∃ i, a i = x}) (this) (-ε) (by linarith)
+/- `Real.add_neg_lt_sSup` provides almost exactly what we need with a little adjustment. -/
+      obtain ⟨aN, ⟨⟨N, hN⟩, _⟩⟩ := Real.add_neg_lt_sSup (this) (show -ε < 0 by linarith)
       use N
-      dsimp
+/- using just `dsimp` works here, but restricting to `only [L]` avoids searching through
+irrelevant definitional equality lemmas. this is used later aswell -/
+      dsimp only [L]
       linarith
   obtain ⟨N, hN⟩ := this
+/- now that we have `a N > L - ε`, we know that for any `n ≥ N`, `L - ε < a n ≤ L`, and
+so we can show convergence -/
   use N
   intro n hn
+/- an absolute value `|x|` is bounded iff both `-x` and `x` are bounded -/
   rw [abs_sub_lt_iff]
   constructor
-  · dsimp
+  · dsimp only [L]
+/- more lemmas involve expressions using only `+`'s and not `-`'s so it is
+helpful to change the goal to this form -/
     rw [sub_lt_iff_lt_add]
+/- proving the following fact is obvious in normal mathematics, but lean does
+not know anything about the set in question. we have not told it that it is
+bounded or non empty, so a supremum may not exist -/
     calc a n ≤ sSup {x | ∃ i, a i = x} := by
-          · specialize hM (show n ≤ n + 1 by linarith)
-            set s := {x | ∃ i, a i = x} with hs
+          · specialize hM (Nat.le_add_right n 1)
+            let s := {x | ∃ i, a i = x}
             have hsbound : BddAbove s := by
               · obtain ⟨R, hR⟩ := hB
                 use R
                 intro i ⟨k, hk⟩
                 rw [← hk]
                 exact le_of_abs_le (hR k)
+/- whilst `ℝ` is not a `CompleteLattice` (every non empty set has a supremum + infimum),
+it is a `ConditionallyCompleteLattice`, (non empty sets have suprema/imfima if they are
+bounded above/bellow) and lean infers this. `le_csSup_of_le` requires a proof that the
+set is bounded above in order to say anything useful about the `sSup` -/
             exact le_csSup_of_le hsbound (by use n + 1) hM
     _ < ε + sSup {x | ∃ i, a i = x} := by exact lt_add_of_pos_left (sSup {x | ∃ i, a i = x}) hε
+/- the next goal is much more abvious as it just relies on the fact that `a` is monotone -/
   · rw [sub_lt_comm]
     calc L - ε < a N := by exact hN
     _ ≤ a n := by exact hM hn
 
 /- for real numbers, cauchy and convergence criterion are equivalent -/
 theorem cauchy_iff_convergent (a : ℕ → ℝ) : converges a ↔ cauchy a := by
+/- `constructor` allows us to split the goal into the two directions -/
   constructor
+/- for both goals, we need to introduce a hypothesis, and this can be done
+using the `<;>` notation -/
   <;> intro h
   · intro ε hε
     obtain ⟨L, hL⟩ := h
@@ -201,7 +239,7 @@ lemma partial_monotone (a : ℕ → ℝ) : (∀ (n : ℕ), a n ≥ 0) ↔ Monoto
     linarith
 
 /- The Comparison Test: if an ≤ bn and the sum of bn's converges, then the same is true for an -/
-theorem comparison_test (a b : ℕ → ℝ) (ha : ∀ n, a n ≥ 0) (hab : ∀ n, a n ≤ b n) (hb : sum_conv b) : sum_conv a := by
+theorem Comparison_Test (a b : ℕ → ℝ) (ha : ∀ n, a n ≥ 0) (hab : ∀ n, a n ≤ b n) (hb : sum_conv b) : sum_conv a := by
   rw [sum_conv_def]
   apply mono_bounded_conv
   constructor
@@ -227,7 +265,7 @@ theorem comparison_test (a b : ℕ → ℝ) (ha : ∀ n, a n ≥ 0) (hab : ∀ n
 
 /- The Sandwich Test: if cn ≤ an ≤ bn and the sums of both the cn and bn converges, then the same
 is true for an  -/
-theorem sandwich_test (a b c : ℕ → ℝ) (hca : ∀ n, c n ≤ a n) (hab : ∀ n, a n ≤ b n) (hc : sum_conv c)
+theorem Sandwich_Test (a b c : ℕ → ℝ) (hca : ∀ n, c n ≤ a n) (hab : ∀ n, a n ≤ b n) (hc : sum_conv c)
   (hb : sum_conv b) : sum_conv a := by
   rw [sum_conv_def, cauchy_iff_convergent, cauchy_def] at *
   intros ε hε
